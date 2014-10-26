cc5cab5fd08f87c91be9b758700d0e3fba2c9bb1
Robert McGibbon <rmcgibbo@gmail.com>
2014-10-26 14:44:18 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,62 @@
+import sys
+import versioneer
+import subprocess
+from distutils.spawn import find_executable
 from setuptools import setup, find_packages
 
-import versioneer
 versioneer.VCS = 'git'
 versioneer.versionfile_source = 'osprey/_version.py'
 versioneer.versionfile_build = 'osprey/_version.py'
 versioneer.tag_prefix = '' # tags are like 1.2.0
 versioneer.parentdir_prefix = 'osprey-' # dirname like 'myproject-1.2.0'
 
-setup(
-    name='osprey',
-    author='Robert T. McGibbon',
-    author_email='rmcgibbo@gmail.com',
-    url='https://github.com/rmcgibbo/osprey',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    packages=find_packages(),
-    zip_safe=False,
-    package_data={'osprey': ['data/*']},
-    entry_points={
-        'console_scripts': [
-            'osprey = osprey.main:main',
-        ],
+def main(**kwargs):
+    classifiers = """\
+    Intended Audience :: Science/Research
+    License :: OSI Approved :: Apache Software License
+    Programming Language :: Python
+    Programming Language :: Python :: 3
+    Operating System :: Unix
+    Operating System :: MacOS
+    Operating System :: Microsoft :: Windows
+    Topic :: Scientific/Engineering
+    Topic :: Scientific/Engineering :: Information Analysis
+    """
+    setup(
+        name='osprey',
+        author='Robert T. McGibbon',
+        author_email='rmcgibbo@gmail.com',
+        url='https://github.com/rmcgibbo/osprey',
+        classifiers=[e.strip() for e in classifiers.splitlines()],
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        packages=find_packages(),
+        zip_safe=False,
+        package_data={'osprey': ['data/*']},
+        entry_points={
+            'console_scripts': [
+                'osprey = osprey.main:main',
+            ],
+        },
+        **kwargs
+    )
+
+
+def readme_to_rst():
+    pandoc = find_executable('pandoc')
+    if pandoc is None:
+        return {}
+    long_description = subprocess.check_output(
+        [pandoc, 'README.md', '-t', 'rst'])
+    short_description = long_description.split('\n\n')[1]
+    return {
+        'description': short_description,
+        'long_description': long_description,
     }
-)
+
+
+if __name__ == '__main__':
+    kwargs = {}
+    if 'upload' in sys.argv:
+        kwargs = readme_to_rst()
+    main(**kwargs)

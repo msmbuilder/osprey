b02594c818e906529b8c6dcc5b9e32876a6c709d
Robert McGibbon <rmcgibbo@gmail.com>
2014-10-27 01:40:36 -0700

    test

--- devtools/setup-git-hooks.sh
+++ devtools/setup-git-hooks.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+cd "$(git rev-parse --show-toplevel)"
 echo "setting git pre-commit hook"
-ln -s -f ../../devtools/pre-commit-hook .git/hooks/pre-commit
-ln -s -f ../../devtools/remove-trailing-whitepace-hook.sh .git/hooks/pre-commit
+# ln -s -f ../../devtools/pre-commit-hook .git/hooks/pre-commit
+# ln -s -f ../../devtools/remove-trailing-whitepace-hook.sh .git/hooks/pre-commit
+ln -s -f ../../devtools/pre-commit .git/hooks/pre-commit

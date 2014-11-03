c881b395d93855ba53a9bc5648ff05fa9eb80925
Robert McGibbon <rmcgibbo@gmail.com>
2014-11-03 15:58:29 -0800

    Rescale down to unit hypercube

--- osprey/tests/test_search_space.py
+++ osprey/tests/test_search_space.py
@@ -11,6 +11,7 @@ except ImportError:
     pass
 
 from osprey.search_space import SearchSpace
+from osprey.search_space import IntVariable, FloatVariable, EnumVariable
 
 
 def test_1():
@@ -123,3 +124,44 @@ def test_5_2():
               for _ in xrange(100)]
 
     _run_chi2_test(values, bin_edges)
+
+
+def test_moe_1():
+    v = IntVariable('name', 1, 10)
+    assert 2 == v.point_from_moe(v.point_to_moe(2))
+    assert 1 == v.point_from_moe(v.point_to_moe(1))
+    assert 10 == v.point_from_moe(v.point_to_moe(10))
+
+    assert v.point_to_moe(1) == 0
+    assert v.point_to_moe(10) == 1
+
+
+def test_moe_2():
+    v = FloatVariable('name', 1, 10, None)
+    assert (2-1) / (10-1) == v.point_to_moe(2)
+    assert 0.0 == v.point_to_moe(1)
+    assert 1.0 == v.point_to_moe(10)
+    assert 2.0 == v.point_from_moe(v.point_to_moe(2))
+    assert 1.0 == v.point_from_moe(v.point_to_moe(1))
+    assert 1.0 == v.point_from_moe(v.point_to_moe(0.9))
+    assert 10.0 == v.point_from_moe(v.point_to_moe(10))
+    assert 10.0 == v.point_from_moe(v.point_to_moe(10.1))
+
+
+def test_moe_3():
+    v = FloatVariable('name', 1, 10, 'log')
+    assert 2.0 == v.point_from_moe(v.point_to_moe(2))
+    assert 1.0 == v.point_from_moe(v.point_to_moe(1))
+    assert 1.0 == v.point_from_moe(v.point_to_moe(0.9))
+    assert 10.0 == v.point_from_moe(v.point_to_moe(10))
+    assert 10.0 == v.point_from_moe(v.point_to_moe(10.1))
+
+
+def test_moe_4():
+    v = EnumVariable('name', ['a', 'b', 'c'])
+    assert 'a' == v.point_from_moe(v.point_to_moe('a'))
+    assert 'b' == v.point_from_moe(v.point_to_moe('b'))
+    assert 'c' == v.point_from_moe(v.point_to_moe('c'))
+    assert 0 == v.point_to_moe('a')
+    assert 0.5 == v.point_to_moe('b')
+    assert 1 == v.point_to_moe('c')

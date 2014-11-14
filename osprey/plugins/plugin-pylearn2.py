7cb36a90cc740de0a5f8cc1bb7db120380ff6f0f
skearnes <kearnes@stanford.edu>
2014-11-13 16:40:12 -0800

    Update plugin-pylearn2.py

--- osprey/plugins/plugin-pylearn2.py
+++ osprey/plugins/plugin-pylearn2.py
@@ -6,12 +6,15 @@ import re
 from string import Template
 
 from pylearn2.config import yaml_parse
-from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
+from pylearn2.datasets import Dataset, DenseDesignMatrix
 from pylearn2.train import Train
 
 from sklearn.base import BaseEstimator
+from sklearn.metrics import accuracy_score, mean_squared_error
 
 import theano
+
+from osprey.dataset_loaders import BaseDatasetLoader
 
 
 class Pylearn2Estimator(BaseEstimator):
@@ -28,9 +31,10 @@ class Pylearn2Estimator(BaseEstimator):
     yaml_string : str
         YAML string defining the model.
     """
-    def __init__(self, yaml_string):
+    def __init__(self, yaml_string, **kwargs):
         self.trainer = None
         self.yaml_string = self.check_yaml_string(yaml_string)
+        self.set_params(**kwargs)
 
     def check_yaml_string(self, string):
         """
@@ -48,7 +52,7 @@ class Pylearn2Estimator(BaseEstimator):
         Get mappable parameters from YAML.
         """
         template = Template(self.yaml_string)
-        names = []
+        names = ['yaml_string']  # always include the template
         for match in re.finditer(template.pattern, template.template):
             name = match.group('named') or match.group('braced')
             assert name is not None
@@ -83,7 +87,11 @@ class Pylearn2Estimator(BaseEstimator):
         y : array_like, optional
             Labels.
         """
-        raise NotImplementedError('should be implemented in subclasses')
+        y = np.asarray(y)
+        if y.ndim == 1:
+            return y.reshape((y.size, 1))
+        assert y.ndim == 2
+        return y
 
     def fit(self, X, y=None):
         """
@@ -96,14 +104,42 @@ class Pylearn2Estimator(BaseEstimator):
         y : array_like, optional
             Labels.
         """
-        self.trainer = yaml_parse.load(self.yaml_string % self.get_params())
+        # build trainer
+        params = self.get_params()
+        yaml_string = Template(self.yaml_string).substitute(params)
+        self.trainer = yaml_parse.load(yaml_string)
         assert isinstance(self.trainer, Train)
-        if self.trainer.database is not None:
+        if self.trainer.dataset is not None:
             raise ValueError('Train YAML database must evaluate to None.')
-        self.trainer.database = self._get_dataset(X, y)
+        self.trainer.dataset = self._get_dataset(X, y)
+
+        # update monitoring dataset(s)
+        if hasattr(self.trainer.algorithm, 'monitoring_dataset'):
+            monitoring_dataset = self.trainer.algorithm.monitoring_dataset
+            if len(monitoring_dataset) == 1 and '' in monitoring_dataset:
+                monitoring_dataset[''] = self.trainer.dataset
+            else:
+                monitoring_dataset['train'] = self.trainer.dataset
+            self.trainer.algorithm._set_monitoring_dataset(monitoring_dataset)
+        else:
+            self.trainer.algorithm._set_monitoring_dataset(
+                self.trainer.dataset)
+
+        # run main loop
         self.trainer.main_loop()
 
-    def _predict(self, X):
+    def predict(self, X):
+        """
+        Get model predictions.
+
+        Parameters
+        ----------
+        X : pylearn2.datasets.Dataset
+            Test dataset.
+        """
+        return self._predict(X)
+
+    def _predict(self, X, method='fprop'):
         """
         Get model predictions.
 
@@ -112,18 +148,33 @@ class Pylearn2Estimator(BaseEstimator):
 
         Parameters
         ----------
-        X : pylearn2.datasets.Dataset
+        X : array_like
             Test dataset.
+        method : str
+            Model method to call for prediction.
         """
         X_sym = self.trainer.model.get_input_space().make_theano_batch()
-        y_sym = self.trainer.model.fprop(X_sym)
+        y_sym = getattr(self.trainer.model, method)(X_sym)
         f = theano.function([X_sym], y_sym)
         return f(X)
+
+    def score(self, X, y):
+        """
+        Score predictions.
+
+        Parameters
+        ----------
+        X : array_like
+            Test examples.
+        y : array_like, optional
+            Labels.
+        """
+        raise NotImplementedError('should be implemented in subclasses')
 
 
 class Pylearn2Classifier(Pylearn2Estimator):
     """
-    Pylearn2 classifier.
+    pylearn2 classifier.
     """
     def _get_labels(self, y):
         """
@@ -136,7 +187,11 @@ class Pylearn2Classifier(Pylearn2Estimator):
         """
         y = np.asarray(y)
         assert y.ndim == 1
-        return y
+        # convert to one-hot
+        oh = np.zeros((y.size, np.unique(y).size), dtype=float)
+        for i, label in enumerate(y):
+            oh[i, label] = 1.
+        return oh
 
     def predict_proba(self, X):
         """
@@ -160,33 +215,91 @@ class Pylearn2Classifier(Pylearn2Estimator):
         """
         return np.argmax(self._predict(X), axis=1)
 
-
-class Pylearn2Regressor(Pylearn2Estimator):
-    """
-    Pylearn2 regressor.
-    """
-    def _get_labels(self, y):
+    def score(self, X, y):
         """
-        Construct pylearn2 dataset labels.
+        Score predictions.
 
         Parameters
         ----------
+        X : array_like
+            Test examples.
         y : array_like, optional
             Labels.
         """
-        y = np.asarray(y)
-        if y.ndim == 1:
-            return y.reshape((y.size, 1))
-        assert y.ndim == 2
-        return y
+        return accuracy_score(y, self.predict(X))
 
-    def predict(self, X):
+
+class Pylearn2Regressor(Pylearn2Estimator):
+    """
+    pylearn2 regressor.
+    """
+    def score(self, X, y):
         """
-        Get model predictions.
+        Score predictions.
 
         Parameters
         ----------
-        X : pylearn2.datasets.Dataset
-            Test dataset.
+        X : array_like
+            Test examples.
+        y : array_like, optional
+            Labels.
         """
-        return self._predict(X)
+        return mean_squared_error(y, self.predict(X))
+
+
+class Pylearn2Autoencoder(Pylearn2Estimator):
+    """
+    pylearn2 autoencoder.
+    """
+    def _predict(self, X, method='reconstruct'):
+        return super(Pylearn2Autoencoder, self)._predict(X, method)
+
+    def score(self, X, y):
+        """
+        Score predictions.
+
+        Parameters
+        ----------
+        X : array_like
+            Test examples.
+        y : array_like, optional
+            Labels (not used).
+        """
+        return mean_squared_error(X, self.predict(X))
+
+
+class Pylearn2DatasetLoader(BaseDatasetLoader):
+    """
+    pylearn2 dataset loader.
+
+    Parameters
+    ----------
+    yaml_string : str
+        YAML specification of a pylearn2 dataset.
+    one_hot : bool, optional
+        Take argmax of one-hot labels to get classes.
+    """
+    short_name = 'pylearn2_dataset'
+
+    def __init__(self, yaml_string, one_hot=False):
+        self.yaml_string = yaml_string
+        self.one_hot = one_hot
+
+    def load(self):
+        """
+        Load the dataset using pylearn2.config.yaml_parse.
+        """
+        dataset = yaml_parse.load(self.yaml_string)
+        assert isinstance(dataset, Dataset)
+        data = dataset.iterator(mode='sequential', num_batches=1,
+                                data_specs=dataset.data_specs,
+                                return_tuple=True).next()
+        if len(data) == 2:
+            X, y = data
+            y = np.squeeze(y)
+            if self.one_hot:
+                y = np.argmax(y, axis=1)
+        else:
+            X = data
+            y = None
+        return X, y

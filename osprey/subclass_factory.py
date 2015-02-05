59d637feaeed65112e91e65f2db642ed7277a807
Robert McGibbon <rmcgibbo@gmail.com>
2015-02-05 13:34:16 -0800

    Use camel case if desired

--- osprey/subclass_factory.py
+++ osprey/subclass_factory.py
@@ -1,6 +1,9 @@
 from __future__ import print_function, absolute_import, division
 
 import inspect
+import itertools
+import collections
+from six import string_types
 from .utils import join_quoted
 
 
@@ -18,7 +21,8 @@ def init_subclass_by_name(baseclass, short_name, params):
     """
     sc = baseclass.__subclasses__()
     for kls in sc:
-        if kls.short_name == short_name:
+        if kls.short_name == short_name or \
+            (_is_collection(kls.short_name) and short_name in kls.short_name):
             try:
                 return kls(**params)
             except TypeError as e:
@@ -30,22 +34,30 @@ def init_subclass_by_name(baseclass, short_name, params):
                     avail = join_quoted(spec.args[1:])
                     raise RuntimeError(
                         "%s's %s. Available params for this subclass are: %s."
-                        % (kls.short_name, str(e), avail))
+                        % (short_name, str(e), avail))
                 elif 'takes exactly' in str(e):
                     required = join_quoted(spec.args[1:-len(spec.defaults)])
                     raise RuntimeError(
                         "%s's %s. Required params for this subclass are %s."
-                        % (kls.short_name, str(e), required))
+                        % (short_name, str(e), required))
                 elif 'takes at least' in str(e):
                     required = join_quoted(spec.args[1:-len(spec.defaults)])
                     optional = join_quoted(spec.args[-len(spec.defaults):])
                     raise RuntimeError(
                         "%s's %s. Required params for this subclass are: %s. "
                         "Optional params are: %s" % (
-                            kls.short_name, str(e), required, optional))
+                            short_name, str(e), required, optional))
                 # :(
                 raise
 
-    avail_names = ', '.join(str(e.short_name) for e in sc)
-    raise ValueError('"%s" is not a recognized subclass. available subclasses '
+    chain = itertools.chain.from_iterable(
+        e.short_name if _is_collection(e.short_name) else [e.short_name] for e in sc)
+    avail_names = ', '.join(str(n) for n in chain)
+    raise ValueError('"%s" is not a recognized subclass. available names '
                      'are: %s' % (short_name, avail_names))
+
+
+def _is_collection(obj):
+    if isinstance(obj, string_types):
+        return False
+    return isinstance(obj, collections.Iterable)

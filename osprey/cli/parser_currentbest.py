1e1d0195f8e2b73d73fc515b894b35ef3e1b4268
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2015-11-08 17:05:26 -0800

    implemented currentbest command

---
+++ osprey/cli/parser_currentbest.py
@@ -0,0 +1,19 @@
+from __future__ import print_function, absolute_import, division
+from argparse import ArgumentDefaultsHelpFormatter
+
+__author__ = 'muneeb'
+
+
+def func(args, parser):
+    # delay import of the rest of the module to improve `osprey -h` performance
+    from ..execute_currentbest import execute
+    execute(args, parser)
+
+
+def configure_parser(sub_parsers):
+    help = 'Get paramters for the current best model'
+    p = sub_parsers.add_parser('current_best', description=help, help=help,
+                               formatter_class=ArgumentDefaultsHelpFormatter)
+    p.add_argument('config', help='Path to worker config file (yaml)')
+
+    p.set_defaults(func=func)

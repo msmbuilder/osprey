f7ea1503f7bc44a09f7ae349e3ec84cc5da4dc74
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2015-11-08 18:03:44 -0800

    flake8 compliency

--- osprey/execute_currentbest.py
+++ osprey/execute_currentbest.py
@@ -4,11 +4,11 @@ import numpy as np
 from .config import Config
 from .trials import Trial
 
+
 def execute(args, parser):
     config = Config(args.config, verbose=False)
 
     session = config.trials()
-
 
     items = [curr.to_dict() for curr in session.query(Trial).all()]
     if not items:
@@ -17,14 +17,15 @@ def execute(args, parser):
         c_b_m = items[np.argmax([i["mean_test_score"] for i in items])]
         parameter_dict = c_b_m["parameters"]
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        print('Best Current Model  = %f+-%f' %(c_b_m["mean_test_score"],\
-                                                np.std(c_b_m["test_scores"])))
+        print('Best Current Model = %f +- %f' % (c_b_m["mean_test_score"],
+                                                 np.std(c_b_m["test_scores"])))
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         print('PipelineStep\tParamter \t Value')
         for i in config.estimator().steps:
             print(i[0])
             for param in sorted(parameter_dict.keys()):
                 if str(param).startswith(i[0]):
-                    print("\t\t",param.split("__")[1],"\t",parameter_dict[param])
+                    print("\t\t", param.split("__")[1], "\t",
+                          parameter_dict[param])
 
     return

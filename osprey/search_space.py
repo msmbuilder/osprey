13904d954ebaebfc0db526d5a9697d191414937f
Carlos Hernandez <cxh@stanford.edu>
2016-03-16 16:38:18 -0700

    update docstring

--- osprey/search_space.py
+++ osprey/search_space.py
@@ -47,8 +47,14 @@ class SearchSpace(object):
         """An integer-valued dimension bounded between `min` <= x <= `max`.
         Note that the right endpoint of the interval includes `max`.
 
-        The base measure associated with this dimension is a categorical
-        distribution with each weight on each of the integers in [min, max].
+        When `warp` is None, the base measure associated with this dimension
+        is a categorical distribution with each weight on each of the integers
+        in [min, max]. With `warp == 'log'`, the base measure is a uniform
+        distribution on the log of the variable, with bounds at `log(min)` and
+        `log(max)`. This is appropriate for variables that are "naturally" in
+        log-space. Other `warp` functions are not supported (yet), but may be
+        at a later time. Please note that this functionality is not supported
+        for `hyperopt_tpe`.
         """
         min, max = map(int, (min, max))
         if max < min:

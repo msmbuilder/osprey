07e2cb7b7e1528e25f35b92eb8803f8d33e0eeb3
Matthew Harrigan <harrigan@stanford.edu>
2016-03-16 14:43:06 -0700

    Update tags in setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,10 @@ def main(**kwargs):
     Intended Audience :: Science/Research
     License :: OSI Approved :: Apache Software License
     Programming Language :: Python
-    Programming Language :: Python :: 2.6
     Programming Language :: Python :: 2.7
     Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.3
     Programming Language :: Python :: 3.4
+    Programming Language :: Python :: 3.5
     Operating System :: Unix
     Operating System :: MacOS
     Operating System :: Microsoft :: Windows

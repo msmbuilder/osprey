6ef17bb10c3b38a4db02123eaeb7d24e0542668d
Carlos Hernandez <cxh@stanford.edu>
2016-03-17 14:59:09 -0700

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -143,8 +143,7 @@ html_theme_path = [
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# TODO: include logo
-# html_logo = '_static/logo-200px.png'
+html_logo = '_static/osprey.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32

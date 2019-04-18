9a52dd617f30fee35c627e3362c48475bd3dc7e3
Carlos Xavier Hernández <cxh@stanford.edu>
2019-04-18 10:18:36 -0400

    sphinx b4

--- devtools/travis-ci/build_docs.sh
+++ devtools/travis-ci/build_docs.sh
@@ -17,7 +17,8 @@ conda install --yes --file docs/requirements.txt
 # sphinx_rtd_theme's latest releases are not available
 # neither is msmb_theme
 # neither is sphinx > 1.3.1 (fix #1892 autodoc problem)
-pip install -I sphinx==1.3.5 sphinx_rtd_theme==0.1.9 msmb_theme==1.2.0
+pip install -I sphinx==1.3.5
+pip install -I sphinx_rtd_theme==0.1.9 msmb_theme==1.2.0
 
 # Make docs
 cd docs && make html && cd -
